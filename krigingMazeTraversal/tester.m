d3fdeb94f6ddf4a2a052750dcd5545379bca975f
raaidkabir <kabiraaid@gmail.com>
2023-09-13 11:53:52 -0400

    added multirobot

--- krigingMazeTraversal/tester.m
+++ krigingMazeTraversal/tester.m
@@ -1,10 +1,10 @@
-clc;
-clear;
-close all;
+clc; clear; close all;
+addpath("../Astar/","../variogramfit/","../variogram/", "../kriging/", "../FMINSEARCHBND/FMINSEARCHBND/","../perlin_matlab/")
 load("perlin.mat")
 A = 1;
-B = 2;
+B = 10;
 C = 0.01;
 D = 1;
+E = 0.03;
 numIter = 500;
-KrigingTraverseMaze(I, A, B, C, D, numIter)
+KrigingTraverseMaze_multirobot(I, A, B, C, D, E, numIter, 3)
